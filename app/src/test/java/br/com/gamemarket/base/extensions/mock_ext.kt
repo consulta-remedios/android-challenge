ea0bea2d659ef6f0a2bf89a6fb1e84dd2a3760bb
trancosowiliam <wileleh@gmail.com>
2019-05-18 13:29:16 -0300

    new unit test

--- app/src/test/java/br/com/gamemarket/base/extensions/mock_ext.kt
+++ app/src/test/java/br/com/gamemarket/base/extensions/mock_ext.kt
@@ -2,6 +2,6 @@ package com.consultaremedios.base.extensions
 
 import org.mockito.Mockito
 
-inline fun <reified T> mock(): Lazy<T> = lazy {
+inline fun <reified T> mockito(): Lazy<T> = lazy {
     Mockito.mock(T::class.java)
 }
