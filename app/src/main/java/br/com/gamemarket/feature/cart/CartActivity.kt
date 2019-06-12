1349a443fb59422d792794c9bd5ed10030d3b808
lucas-marciano <lucas.almeida.marciano@gmail.com>
2019-06-12 19:24:36 -0300

    (feat): beggin implementation.

---
+++ app/src/main/java/br/com/gamemarket/feature/cart/CartActivity.kt
@@ -0,0 +1,13 @@
+package br.com.gamemarket.feature.cart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.gamemarket.R
+
+class CartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
